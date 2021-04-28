4ffff6d5513b8d7ec22b0d48a6a1af15838426ba
Aaron Saksik <aaron.saksik@ynov.com>
2021-04-28 20:37:52 +0200

    update app.js test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const app = require('./services/express.service');
 const db = require('./services/mongoose.service');
-const offer = require('../src/controllers/offers.controller');
+// const offer = require('../src/controllers/offers.controller');
 
 app.start();
 db.connectDb();
 
-setInterval(offer.updateValidated, 10000);
+// setInterval(offer.updateValidated, 10000);
 
 
 // heroku login
